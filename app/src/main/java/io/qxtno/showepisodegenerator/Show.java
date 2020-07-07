1cd6c40e7b52ac7dac81b5a7b81a71fdc5c41c9c
Karol <qxtno@outlook.com>
2020-07-07 20:48:52 +0200

    Revert "Getting started with setting up a database to store shows."
    This reverts commit 9bdd7965

--- app/src/main/java/io/qxtno/showepisodegenerator/Show.java
+++ app/src/main/java/io/qxtno/showepisodegenerator/Show.java
@@ -6,23 +6,8 @@ import android.os.Parcelable;
 class Show implements Parcelable {
     private String title;
     private int[] seasons;
-    private String fav;
-    private String key_id;
 
-    Show(String title, int[] seasons, String fav, String key_id) {
-        this.title = title;
-        this.seasons = seasons;
-        this.fav = fav;
-        this.key_id = key_id;
-    }
-
-    public Show(String title, int[] seasons, String fav) {
-        this.title = title;
-        this.seasons = seasons;
-        this.fav = fav;
-    }
-
-    public Show(String title, int[] seasons) {
+    Show(String title, int[] seasons) {
         this.title = title;
         this.seasons = seasons;
     }
@@ -30,10 +15,7 @@ class Show implements Parcelable {
     protected Show(Parcel in) {
         title = in.readString();
         seasons = in.createIntArray();
-        fav = in.readString();
-        key_id = in.readString();
     }
-
 
     public static final Creator<Show> CREATOR = new Creator<Show>() {
         @Override
@@ -55,34 +37,6 @@ class Show implements Parcelable {
         return seasons;
     }
 
-    public String getFav() {
-        return fav;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setSeasons(int[] seasons) {
-        this.seasons = seasons;
-    }
-
-    public void setFav(String fav) {
-        this.fav = fav;
-    }
-
-    public String getKey_id() {
-        return key_id;
-    }
-
-    public void setKey_id(String key_id) {
-        this.key_id = key_id;
-    }
-
-    public static Creator<Show> getCREATOR() {
-        return CREATOR;
-    }
-
     @Override
     public int describeContents() {
         return 0;
@@ -92,7 +46,5 @@ class Show implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(title);
         dest.writeIntArray(seasons);
-        dest.writeString(fav);
-        dest.writeString(key_id);
     }
 }
