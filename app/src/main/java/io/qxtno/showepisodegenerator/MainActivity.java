65018ab6b734c4b3ad6226fd249c7a8be13a5725
Karol <qxtno@outlook.com>
2020-07-14 18:17:15 +0200

    Added dark theme support.

--- app/src/main/java/io/qxtno/showepisodegenerator/MainActivity.java
+++ app/src/main/java/io/qxtno/showepisodegenerator/MainActivity.java
@@ -4,16 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -23,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setProperTheme();
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -76,5 +83,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+   private void setProperTheme(){
+
+       SharedPreferences prefs = getSharedPreferences("THEME", Context.MODE_PRIVATE);
+       boolean r1 = prefs.getBoolean("theme_1", false);
+       boolean r2 = prefs.getBoolean("theme_2", false);
+       boolean r3 = prefs.getBoolean("theme_3", false);
+       boolean r4 = prefs.getBoolean("theme_4", false);
+
+       if (r1) {
+           AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+       } else if (r2) {
+           AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+       } else if (r3) {
+           AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+       } else if (r4) {
+           AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+       }
     }
 }
--- app/src/main/java/io/qxtno/showepisodegenerator/SettingsFragment.java
+++ app/src/main/java/io/qxtno/showepisodegenerator/SettingsFragment.java
@@ -1,23 +1,108 @@
 package io.qxtno.showepisodegenerator;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
-public class SettingsFragment extends Fragment {
+import java.util.Objects;
 
+public class SettingsFragment extends Fragment {
+    RadioGroup themeGroup;
+    RadioButton radio1;
+    RadioButton radio2;
+    RadioButton radio3;
+    RadioButton radio4;
+    SharedPreferences prefs;
+    SharedPreferences.Editor settingsEditor;
+
+    @SuppressLint("CommitPrefEdits")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.getDefaultNightMode());
+
+        themeGroup = view.findViewById(R.id.radio_group);
+        radio1 = view.findViewById(R.id.radio_light);
+        radio2 = view.findViewById(R.id.radio_dark);
+        radio3 = view.findViewById(R.id.radio_auto_battery_save);
+        radio4 = view.findViewById(R.id.radio_system_theme);
+
+        prefs = Objects.requireNonNull(getActivity()).getSharedPreferences("THEME", Context.MODE_PRIVATE);
+
+        boolean r1 = prefs.getBoolean("theme_1", false);
+        boolean r2 = prefs.getBoolean("theme_2", false);
+        boolean r3 = prefs.getBoolean("theme_3", false);
+        boolean r4 = prefs.getBoolean("theme_4", false);
+
+        if (r1) {
+            radio1.setChecked(prefs.getBoolean("theme_1", false));
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        } else if (r2) {
+            radio2.setChecked(prefs.getBoolean("theme_2", false));
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else if (r3) {
+            radio3.setChecked(prefs.getBoolean("theme_3", false));
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+        } else if (r4) {
+            radio4.setChecked(prefs.getBoolean("theme_4", false));
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        }
+
+        themeGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                prefs = Objects.requireNonNull(getContext()).getSharedPreferences("THEME", Context.MODE_PRIVATE);
+                settingsEditor = prefs.edit();
+                switch (checkedId) {
+                    case R.id.radio_light:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        settingsEditor.putBoolean("theme_1", true);
+                        settingsEditor.putBoolean("theme_2", false);
+                        settingsEditor.putBoolean("theme_3", false);
+                        settingsEditor.putBoolean("theme_4", false);
+                        settingsEditor.apply();
+                        break;
+                    case R.id.radio_dark:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        settingsEditor.putBoolean("theme_1", false);
+                        settingsEditor.putBoolean("theme_2", true);
+                        settingsEditor.putBoolean("theme_3", false);
+                        settingsEditor.putBoolean("theme_4", false);
+                        settingsEditor.apply();
+                        break;
+                    case R.id.radio_auto_battery_save:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+                        settingsEditor.putBoolean("theme_1", false);
+                        settingsEditor.putBoolean("theme_2", false);
+                        settingsEditor.putBoolean("theme_3", true);
+                        settingsEditor.putBoolean("theme_4", false);
+                        break;
+                    case R.id.radio_system_theme:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                        settingsEditor.putBoolean("theme_1", false);
+                        settingsEditor.putBoolean("theme_2", false);
+                        settingsEditor.putBoolean("theme_3", false);
+                        settingsEditor.putBoolean("theme_4", true);
+                        break;
+                }
+                settingsEditor.apply();
+            }
+        });
+
         return view;
     }
-
 }
