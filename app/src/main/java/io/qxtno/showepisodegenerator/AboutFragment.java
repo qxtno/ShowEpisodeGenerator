b630bfe9f9e496022b4a6384cd2d90d23b63b7ed
Karol <qxtno@outlook.com>
2020-09-06 11:06:13 +0200

    Added description to about fragment.

--- app/src/main/java/io/qxtno/showepisodegenerator/AboutFragment.java
+++ app/src/main/java/io/qxtno/showepisodegenerator/AboutFragment.java
@@ -1,9 +1,12 @@
 package io.qxtno.showepisodegenerator;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +21,28 @@ public class AboutFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
 
         ((MainActivity)requireActivity()).toolbar.setTitle(R.string.about);
+
+        Button github = view.findViewById(R.id.github_button);
+        Button email = view.findViewById(R.id.email_button);
+
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/qxtno"));
+                startActivity(browserIntent);
+            }
+        });
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String subject = "Suggestion regarding " + getResources().getString(R.string.app_name);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri data = Uri.parse("mailto:qxtno@outlook.com?subject=" + Uri.encode(subject));
+                intent.setData(data);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
